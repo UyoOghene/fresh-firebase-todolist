7740e5b39a5be03d3529f7afbcaea38f2724f692
Uyooghene <uyooghene01@gmail.com>
2024-07-13 08:29:15 +0100

    good

--- script.js
+++ script.js
@@ -1,4 +1,177 @@
 
+// import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js";
+// import { getAuth, GoogleAuthProvider, signInWithPopup, signOut } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-auth.js";
+// import { getDatabase, ref, onValue, push, remove, update } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js";
+
+// window.env = {
+//     New_api_key: "AIzaSyCAZPZFJ3UZ2NY9J6O3OhEh_RDedKTkVco"
+// };
+
+// const firebaseConfig = {
+//     apiKey: window.env.New_api_key,
+//     authDomain: "fresh-todolist.firebaseapp.com",
+//     projectId: "fresh-todolist",
+//     storageBucket: "fresh-todolist.appspot.com",
+//     messagingSenderId: "961687776701",
+//     appId: "1:961687776701:web:4b7edef4f97b0c3638b6d4",
+//     databaseURL: "https://fresh-todolist-default-rtdb.firebaseio.com/",
+// };
+
+// const app = initializeApp(firebaseConfig);
+// const auth = getAuth(app);
+// const dataBase = getDatabase(app);
+// auth.languageCode = 'en';
+// const provider = new GoogleAuthProvider();
+// const user = auth.currentUser;
+
+// const googleBtn = document.querySelector('#googleBtn');
+// const loginContainer = document.querySelector('.login-container');
+// const container = document.querySelector('#container');
+// const inputField = document.querySelector('#input-field');
+// const inputForm = document.querySelector('#add-btn-field');
+// const shoppingItemList = document.querySelector('#shopping-item-list');
+// const namebox = document.querySelector('#namebox');
+// const imgbox = document.querySelector('#imgbox');
+// const logoutBtn = document.querySelector('#logoutBtn');
+
+// googleBtn.addEventListener('click', () => {
+//     signInWithPopup(auth, provider)
+//         .then((result) => {
+//             const user = result.user;
+//             console.log(user)
+//             localStorage.setItem('email', user.email);
+//             localStorage.setItem('userStore', JSON.stringify(user));
+//             localStorage.setItem('pic', user.photoURL);
+//             loginContainer.style.display = 'none';
+//             container.style.display = 'flex';
+//             onGoogleLogin();
+//         })
+//         .catch((error) => {
+//             console.error(error.code, error.message);
+//         });
+// });
+
+// function login(e) {
+//     e.preventDefault();
+//     const username = document.getElementById('username').value;
+//     const password = document.getElementById('password').value;
+//     localStorage.setItem('username', username);
+
+//     if (username !== '' && password.length >= 5) {
+//         const usernamestore = (localStorage.getItem('username'));
+//         loginContainer.style.display = 'none';
+//         container.style.display = 'flex';
+//         namebox.textContent = usernamestore;
+//         onGoogleLogin();
+//     } else {
+//         alert('Your password should be at least 5 characters');
+//     }
+// }
+
+// document.querySelector('#login').addEventListener('click', login);
+
+// const addToCart = (e) => {
+//     e.preventDefault();
+//     let name;
+//     const user = JSON.parse(localStorage.getItem('userStore'));
+
+//     if (user && user.displayName) {
+//         name = user.displayName;
+//     } else {
+//         const usernamestore = localStorage.getItem('username');
+//         name = usernamestore;
+//     }
+
+//     const item = inputField.value;
+//     const d = new Date();
+//     const date = d.toLocaleDateString('en-US', {
+//         year: 'numeric',
+//         month: '2-digit',
+//         day: '2-digit'
+//     });
+
+//     if (name !== null && item.trim() !== '') {
+//         push(ref(dataBase, "shoppingList"), { item, user: name, date, completed: false });
+//         inputField.value = '';
+//     }
+// };
+// inputForm.addEventListener('submit', addToCart);
+
+// const onGoogleLogin = () => {
+//     const email = localStorage.getItem('email');
+    
+//     if (email) {
+//         namebox.textContent = email;
+//         const user = JSON.parse(localStorage.getItem('userStore'));
+//         const pic = user.photoURL;
+//         imgbox.setAttribute('src', pic);
+//     }
+// };
+
+// const logout = () => {
+//         signOut(auth).then(() => {
+//           localStorage.removeItem('email');
+//           localStorage.removeItem('username');
+//           localStorage.removeItem('userStore');
+//           window.location.href = './index.html';
+//         }).catch((error) => {
+//           console.error('Sign out error', error);
+//         });
+//       };
+
+// logoutBtn.addEventListener('click', logout);
+
+// onValue(ref(dataBase, "shoppingList"), (snapshot) => {
+//     if (snapshot.exists()) {
+//         const items = snapshot.val();
+//         shoppingItemList.innerHTML = '';
+//         const table = document.createElement('table');
+//         table.setAttribute('border', '1');
+//         table.setAttribute('id', 'table');
+//         shoppingItemList.appendChild(table);
+//         const headerRow = document.createElement('tr');
+//         headerRow.setAttribute('id', 'headerRow');
+//         const headers = ['Item', 'User', 'Date'];
+//         headers.forEach(headerText => {
+//             const th = document.createElement('th');
+//             th.setAttribute('id', 'th');
+//             th.textContent = headerText;
+//             headerRow.appendChild(th);
+//         });
+//         table.appendChild(headerRow);
+
+//         Object.keys(items).forEach(key => {
+//             const { item, user, date, completed } = items[key];
+//             const row = document.createElement('tr');
+//             row.setAttribute('id', 'row');
+//             table.appendChild(row);
+
+//             [item, user, date].forEach(text => {
+//                 const td = document.createElement('td');
+//                 td.setAttribute('id', 'td');
+//                 td.textContent = text;
+//                 row.appendChild(td);
+//             });
+
+//             if (completed) {
+//                 row.style.textDecoration = 'line-through';
+//             }
+
+//             row.addEventListener('click', () => {
+//                 const exactLocation = ref(dataBase, `shoppingList/${key}`);
+//                 update(exactLocation, { completed: !completed });
+//             });
+
+//             row.addEventListener('dblclick', () => {
+//                 const exactLocation = ref(dataBase, `shoppingList/${key}`);
+//                 remove(exactLocation);
+//             });
+//         });
+//     } else {
+//         shoppingItemList.innerHTML = 'No items on the list yet';
+//     }
+// });
+
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js";
 import { getAuth, GoogleAuthProvider, signInWithPopup, signOut } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-auth.js";
 import { getDatabase, ref, onValue, push, remove, update } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js";
@@ -27,11 +200,15 @@ const user = auth.currentUser;
 const googleBtn = document.querySelector('#googleBtn');
 const loginContainer = document.querySelector('.login-container');
 const container = document.querySelector('#container');
+const dropdown = document.querySelector('.dropdown');
+const dropbtn = document.querySelector('.dropbtn');
+const dropDownContent = document.querySelector('.dropdown-content');
 const inputField = document.querySelector('#input-field');
 const inputForm = document.querySelector('#add-btn-field');
 const shoppingItemList = document.querySelector('#shopping-item-list');
 const namebox = document.querySelector('#namebox');
 const imgbox = document.querySelector('#imgbox');
+const picturebox = document.querySelector('#picturebox');
 const logoutBtn = document.querySelector('#logoutBtn');
 
 googleBtn.addEventListener('click', () => {
@@ -61,8 +238,11 @@ function login(e) {
         const usernamestore = (localStorage.getItem('username'));
         loginContainer.style.display = 'none';
         container.style.display = 'flex';
+        dropbtn.removeChild(picturebox);
+        dropbtn.appendChild(namebox)
         namebox.textContent = usernamestore;
-        onGoogleLogin();
+        namebox.style.display = 'flex';
+        imgbox.style.display = 'none';
     } else {
         alert('Your password should be at least 5 characters');
     }
@@ -101,9 +281,12 @@ const onGoogleLogin = () => {
     const email = localStorage.getItem('email');
     
     if (email) {
-        namebox.textContent = email;
+        namebox.style.display = 'flex';
+        imgbox.style.display = 'flex';
         const user = JSON.parse(localStorage.getItem('userStore'));
         const pic = user.photoURL;
+        const name = user.displayName;
+        namebox.textContent = name;
         imgbox.setAttribute('src', pic);
     }
 };
@@ -168,6 +351,6 @@ onValue(ref(dataBase, "shoppingList"), (snapshot) => {
             });
         });
     } else {
-        shoppingItemList.innerHTML = 'No items on the list yet';
+        shoppingItemList.innerHTML = 'No items here... add an item';
     }
 });
